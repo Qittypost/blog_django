a681e5a88c7c842b11c0fd6aa98757dac12a481d
Vladtopchik <Vladkyriniq@gmail.com>
2024-11-12 15:30:43 +0200

    Changed views.py and post_list.html

--- core/blog/views.py
+++ core/blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def get_posts(request):
     posts = Post.objects.all()
-    return render(request, 'posts/post_list.html', {'posts': posts})
+    return render(request, 'post_list.html', {'posts': posts})
